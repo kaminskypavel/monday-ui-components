0ff22109cb7527779062a7c8ad9675da3b5893a4
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-08-13 15:25:52 +0300

    build: add babelrc

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = (api) => {
+    api.cache(true);
+
+    return {
+        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+        plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+    };
+};
