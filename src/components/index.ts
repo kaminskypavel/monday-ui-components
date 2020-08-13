01e5bf72fb26637d63b2638cdb8db3113c629b37
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-08-14 00:48:59 +0300

    refactor: change export so tsc will include all the components in build

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable import/export
-
-import Badge from "./Badge";
-import Button from "./Button";
-
-export {Badge, Button};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export * from "./components";
+// eslint-disable import/export
+
+import Badge from "./components/Badge";
+import Button from "./components/Button";
+
+export {Badge, Button};
