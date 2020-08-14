a55d17eefbc0c186198d9b0cb0d34c1fbeff6811
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-08-14 12:01:21 +0300

    refactor: move Badge component to styled-components

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "fontsource-roboto";
-import "./badge.scss";
+import styled from "styled-components";
+import {color} from "../shared/styles";
 
 type Props = {
   size: "big" | "small" | "notification";
@@ -8,11 +9,57 @@ type Props = {
   count: number;
 };
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BadgeSpan = styled.div<Pick<Props, "type" | "size">>`
+  display: inline-block;
+  padding: 4px 10px 4px 10px;
+  border-radius: 50%;
+  text-align: center;
+  font-family: Roboto, monospace;
+
+  ${({type}) => {
+    switch (type) {
+      case "dark":
+        return `
+            background-color: ${color.dark};
+            border: ${color.dark} 1px solid;
+            color:  ${color.white}
+        `;
+      case "notification":
+        return `
+            background-color: #F65F7C;
+            border: #F65F7C 1px solid;
+            color:  ${color.white}
+        `;
+      case "secondary":
+        return `
+            background-color: ${color.white};
+            border: ${color.primary} 1px solid;
+            color:  ${color.primary}
+        `;
+      case "primary":
+      default:
+        return `
+            background-color: ${color.primary};
+            border: ${color.primary} 1px solid;
+            color:  ${color.white}
+        `;
+    }
+  }}
+`;
+
 export const Badge: React.FunctionComponent<Props> = (props: Props) => {
   const {count, size, type = "primary"} = props;
   return (
-    <div className="wrapper">
-      <span className={`badge ${type} ${size}`}>{count}</span>
-    </div>
+    <Wrapper>
+      <BadgeSpan type={type} size={size}>
+        {count}
+      </BadgeSpan>
+    </Wrapper>
   );
 };
