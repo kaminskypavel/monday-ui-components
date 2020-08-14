ab3d0a7986bd7807251d93a1e0561db1328445b5
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-08-14 13:35:23 +0300

    refactor(storybook): inline stories in the components folders

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,54 +1,52 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    // addons: [
-    //     '@storybook/addon-actions',
-    //     '@storybook/addon-links',
-    //     '@storybook/addon-controls',
-    //     '@storybook/addon-knobs',
-    //     {
-    //         name: '@storybook/addon-docs',
-    //         options: {
-    //             configureJSX: true,
-    //         },
-    //     },
-    // ],
-    addons: ['@storybook/addon-essentials'],
-    stories: ['../src/stories/*.stories.tsx'],
-    webpackFinal: async config => {
-        config.module.rules = config.module.rules.filter(rule => {
-            if (rule.test instanceof RegExp && rule.test.test('.scss')) {
-                rule.exclude = /\.(stories|story).s[ca]ss$/;
-            }
-            return rule;
-        });
+  // addons: [
+  //     '@storybook/addon-actions',
+  //     '@storybook/addon-links',
+  //     '@storybook/addon-controls',
+  //     '@storybook/addon-knobs',
+  //     {
+  //         name: '@storybook/addon-docs',
+  //         options: {
+  //             configureJSX: true,
+  //         },
+  //     },
+  // ],
+  addons: ["@storybook/addon-essentials"],
+  stories: ["../src/**/*.stories.tsx"],
+  webpackFinal: async (config) => {
+    config.module.rules = config.module.rules.filter((rule) => {
+      if (rule.test instanceof RegExp && rule.test.test(".scss")) {
+        rule.exclude = /\.(stories|story).s[ca]ss$/;
+      }
+      return rule;
+    });
 
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      include: path.resolve(__dirname, "../src"),
+      use: [
+        require.resolve("ts-loader"),
+        {
+          loader: require.resolve("react-docgen-typescript-loader"),
+          options: {
+            // Provide the path to your tsconfig.json so that your stories can
+            // display types from outside each individual story.
+            tsconfigPath: path.resolve(__dirname, "../tsconfig.json")
+          }
+        }
+      ]
+    });
 
-        config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            include: path.resolve(__dirname, "../src"),
-            use: [
-                require.resolve("ts-loader"),
-                {
-                    loader: require.resolve('react-docgen-typescript-loader'),
-                    options: {
-                        // Provide the path to your tsconfig.json so that your stories can
-                        // display types from outside each individual story.
-                        tsconfigPath: path.resolve(__dirname, "../tsconfig.json"),
-                    },
-                },
-            ],
-        });
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../src")
+    });
 
-        // Make whatever fine-grained changes you need
-        config.module.rules.push({
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-            include: path.resolve(__dirname, "../src"),
-        });
-
-
-        config.resolve.extensions.push('.ts', '.tsx');
-        return config;
-    },
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  }
 };
--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Badge from "../components/Badge";
+import Badge from ".";
 
 export default {
   title: "Components/Badge",
