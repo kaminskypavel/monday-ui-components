2af2b2460cb3f05bf932ad51ece8feb10aa819ef
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-08-26 15:56:18 +0300

    fix: add pills to demo [skip ci]

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {Badge, Button} from "monday-ui-components";
+import {Badge, Button, Pill, PillsGroup} from "monday-ui-components";
 
 function App() {
   const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
+        <PillsGroup>
+          <Pill label="Days" active />
+          <Pill label="Weeks" />
+          <Pill label="Months" />
+        </PillsGroup>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           a demo of{" "}
